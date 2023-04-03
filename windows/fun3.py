8d61e2a4df1182b747b7b41b00d8b6640f642d76
TaQuangSang69 <sangtqs.bi12-382@st.usth.edu.vn>
2023-04-03 11:47:36 +0700

    Changed functionality from export to import item

--- windows/fun3.py
+++ windows/fun3.py
@@ -6,9 +6,9 @@ import sqlite3
 class fun3:
     def __init__(self, master):
         self.master = master
-        self.header = Label(self.master, text="Export Furniture", bg="white", fg="black", font=("Times New Roman", 20))
+        self.header = Label(self.master, text="IMPORT FURNITURE", bg="#009966", fg="#FFFF33", font=("Times New Roman", 20))
         self.header.pack(side=TOP, fill = BOTH)
-        self.cart = [] # Stores the ID of item's you want to purchase and how many of that item to buy (together as a dict,, add in the "item_index" key as well)
+        self.cart = [] # Stores the ID of item's you want to sell and how many of that item to sell (together as a dict,, add in the "item_index" key as well)
         self.item_index = 0 # The position of an item on the treeview, unique for each new item in the treeview
         self.fun3_gui()
 
@@ -32,21 +32,21 @@ class fun3:
         self.Amount.place(x=350, y=100, height=40, width=200)
 
         # Buttons
-        self.search = Button(self.mainFrame, text="Add to cart", bg="#EE0000", fg="white", font=("Times New Roman", 20), command=self.add_furniture)
-        self.search.place(x= 570, y=70, height=40, width=150)
-        self.Export = Button(self.mainFrame, text="Confirm", bg="#EE0000", fg="white", font=("Times New Roman", 20), command=self.export_furniture)
+        self.search = Button(self.mainFrame, text="Add to inventory", bg="#EE0000", fg="white", font=("Times New Roman", 20), command=self.add_furniture)
+        self.search.place(x= 570, y=70, height=40, width=230)
+        self.Export = Button(self.mainFrame, text="Confirm", bg="#EE0000", fg="white", font=("Times New Roman", 20), command=self.import_furniture)
         self.Export.place(x= 290, y=550, height=40, width=200)
         
         # Treeview is the cart
-        self.tree = tkinter.ttk.Treeview(self.mainFrame, columns=('ID','Name', 'Export price', 'Quantity', 'Category'), show='headings')
+        self.tree = tkinter.ttk.Treeview(self.mainFrame, columns=('ID','Name', 'Import price', 'Quantity', 'Category'), show='headings')
         self.tree.column('ID', width=10)
         self.tree.column('Name', width=10)
-        self.tree.column('Export price', width=10)
+        self.tree.column('Import price', width=10)
         self.tree.column('Quantity', width=10)
         self.tree.column('Category', width=10)
         self.tree.heading('ID', text='ID')        
         self.tree.heading('Name', text='Name')        
-        self.tree.heading('Export price', text='Export price')
+        self.tree.heading('Import price', text='Import price')
         self.tree.heading('Quantity', text='Quantity')
         self.tree.heading('Category', text='Category')
         self.tree.place(x=22, y=160, height=370, width=700)
@@ -69,23 +69,29 @@ class fun3:
                 db_connection = sqlite3.connect("store.db")
                 db_cursor = db_connection.cursor()
                 
-
                 try:
                     # Search for the row with self.ID
                     search_item_query = f"SELECT * FROM inventory WHERE id={int(self.furniture_ID)};"
                     db_cursor.execute(search_item_query)                               
                 except sqlite3.OperationalError: # This except block will occur if the entry field ID is not an integer
                     tkinter.messagebox.showerror("Failed", f"Cannot find furniture with the ID {self.furniture_ID}")
-                else: # If nothing goes wrong then execut this code block
+                else: # If nothing goes wrong then execute this code block
                     if int(self.furniture_amount) > 0:
-                        # Here, fetchone() is used to gather data of the row found afer execute the SQLite command in search_item_query
+                        # Here, fetchone() is used to gather data of the row found after execute the SQLite command in search_item_query
                         item_data = db_cursor.fetchone()
+                         # Get the item's current stock in the inventory...
+                        get_item_stock_query = f"SELECT stock FROM inventory WHERE id={int(self.furniture_ID)};"
+                        item_current_stock = db_cursor.execute(get_item_stock_query).fetchone()
+                        # ...and the item's name as well
+                        get_item_name_query = f"SELECT name FROM inventory WHERE id={int(self.furniture_ID)}"
+                        item_name = db_cursor.execute(get_item_name_query).fetchone()
+                        
                         if item_data != None: # If the item is found in inventory, do the following...
                             # If the cart is empty then add the item into the list
                             if len(self.cart) == 0:                
                                 # Create a new dictionary and increment the item_index by 1
                                 new_item_dict = {}
-                                new_item_dict.update({"id":item_data[0], "buy_quantity":int(self.furniture_amount), "export_price":item_data[4],"item_index":self.item_index})
+                                new_item_dict.update({"id":item_data[0], "sell_quantity":int(self.furniture_amount), "import_price":item_data[4],"item_index":self.item_index})
                                 self.item_index += 1
 
                                 # Add the item into the treeview according to the item_index and the 'cart'
@@ -95,35 +101,25 @@ class fun3:
                                 # Let the user know that the item is added into the 'cart'
                                 tkinter.messagebox.showinfo("Success", "Furniture added to cart successfully")
                             else:
-                                # Iterate through the cart everytime the user click the button if the cart is not empty
+                                # Iterate through the cart every time the user click the button if the cart is not empty
                                 for furniture in self.cart:
                                     item_id = furniture.get("id")
-                                    # Get the item's current stock in the inventory...
-                                    get_item_stock_query = f"SELECT stock FROM inventory WHERE id={item_id};"
-                                    item_current_stock = db_cursor.execute(get_item_stock_query).fetchone()
-                                    # ...and the item's name as well
-                                    get_item_name_query = f"SELECT name FROM inventory WHERE id={item_id}"
-                                    item_name = db_cursor.execute(get_item_name_query).fetchone()
                                         
                                     # Check if the user type in a new furniture ID
                                     if item_id != int(self.furniture_ID):
-                                        # Check if the user's request can be satisfied with the current stock of a specific item
-                                        if int(self.furniture_amount) <= item_current_stock[0]:
-                                            # Create a new dictionary and increment the item_index by 1
-                                            new_item_dict = {}
-                                            new_item_dict.update({"id":item_data[0], "buy_quantity":int(self.furniture_amount), "export_price":item_data[4], "item_index":self.item_index})
-                                            self.item_index += 1
+                                        # Create a new dictionary and increment the item_index by 1
+                                        new_item_dict = {}
+                                        new_item_dict.update({"id":item_data[0], "buy_quantity":int(self.furniture_amount), "export_price":item_data[4], "item_index":self.item_index})
+                                        self.item_index += 1
 
-                                            # Add the item into the treeview according to the item_index and the 'cart'
-                                            self.tree.insert('', self.item_index, values=(item_data[0], item_data[1], item_data[4], self.furniture_amount, item_data[3]))
-                                            self.cart.append(new_item_dict)
-                                            
-                                            # Let the user know that the item is added into the 'cart'
-                                            tkinter.messagebox.showinfo("Success", "Furniture added to cart successfully")
-                                            break
-                                        else:
-                                            tkinter.messagebox.showerror(f"Cannot buy item {item_name[0]}", "There is not enough of that item to buy")
-                                            break
+                                        # Add the item into the treeview according to the item_index and the 'cart'
+                                        self.tree.insert('', self.item_index, values=(item_data[0], item_data[1], item_data[4], self.furniture_amount, item_data[3]))
+                                        self.cart.append(new_item_dict)
+                                        
+                                        # Let the user know that the item is added into the 'cart'
+                                        tkinter.messagebox.showinfo("Success", "Furniture added to cart successfully")
+                                        break
+
                                     else: # And if they type in an ID from one of the items in the 'cart'...
                                         # Calculate the new amount of that item the user wanted to buy
                                         current_buy_quantity = furniture.get("buy_quantity")
@@ -131,19 +127,16 @@ class fun3:
 
                                         # Then the item in the treeview should be updated with the new buy_quantity value,
                                         # assuming that the new_buy_quantity <= item's current stock in inventory     
-                                        if new_buy_quantity <= item_current_stock[0]:                                  
-                                            x = self.tree.get_children()
-                                            self.tree.item(x, values=(item_data[0], item_data[1], item_data[4], new_buy_quantity, item_data[3]))
-                                            furniture.update({"buy_quantity":new_buy_quantity})
-                                            break
-                                        else:
-                                            tkinter.messagebox.showerror(f"Cannot buy item {item_name[0]}", "There is not enough of that item to buy")
-                                            break
+                                        x = self.tree.get_children()
+                                        self.tree.item(x, values=(item_data[0], item_data[1], item_data[4], new_buy_quantity, item_data[3]))
+                                        furniture.update({"buy_quantity":new_buy_quantity})
+                                        break
+                                        
                         else: # if the item is not in inventory, tell the user that the item does not exist in the table 
                             tkinter.messagebox.showerror("Failed", f"Cannot find furniture with the ID {int(self.furniture_ID)}")
                             self.ID.delete(0, END)
                     else:
-                        tkinter.messagebox.showerror("Failed", "Cannot be a nonpositive value")
+                        tkinter.messagebox.showerror("Failed", "Cannot be a non-positive value")
                 finally: 
                     # Close the connection regardless of whatever happened
                     db_cursor.close()
@@ -155,7 +148,7 @@ class fun3:
             tkinter.messagebox.showerror("Error", "Please input furniture ID")
     
     # Manage the export and import of the furniture
-    def export_furniture(self, *args,**kwargs):
+    def import_furniture(self, *args,**kwargs):
         # Establish connection to database
         db_connection = sqlite3.connect("store.db")
         db_cursor = db_connection.cursor()
@@ -173,15 +166,15 @@ class fun3:
                 current_item_stock = db_cursor.execute(get_item_stock_query).fetchone()
 
                 # How many of that item does the user want to buy ?
-                item_buy_quantity = int(item.get("buy_quantity")) 
+                item_buy_quantity = int(item.get("sell_quantity")) 
                 # What is the export price of that item individually?
-                item_export_price = int(item.get("export_price")) 
+                item_import_price = int(item.get("import_price")) 
 
                 # Calculate the transaction fee until the for loop ends
-                transaction_fee += item_buy_quantity * item_export_price
+                transaction_fee += item_buy_quantity * item_import_price
 
                 # Update the item's stock and commit the change to the table
-                update_quantity_query = f"UPDATE inventory SET stock={current_item_stock[0] - item_buy_quantity} WHERE id={item_id};"
+                update_quantity_query = f"UPDATE inventory SET stock={current_item_stock[0] + item_buy_quantity} WHERE id={item_id};"
                 db_cursor.execute(update_quantity_query)
                 db_connection.commit()
             

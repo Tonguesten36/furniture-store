315f623e4fc2608d5b4f41c0729f1db7ea9104b6
Tonguesten36 <tung@tungsoft.net>
2023-04-16 19:51:55 +0700

    Added total_products functionality

--- windows/dashboard.py
+++ windows/dashboard.py
@@ -20,52 +20,67 @@ class dashboard:
         self.activeFrame.pack(side=TOP, fill = BOTH)
         
         # total products
-        
         self.total = Image.open("./images/total.jpg")
         self.total_Resize = self.total.resize((200, 200), Image.ANTIALIAS)
         self.total_Resize = ImageTk.PhotoImage(self.total_Resize)
         self.totalLabel = tk.Button(self.activeFrame, image=self.total_Resize, command=self.total_products)
         self.totalLabel.place(x=35, y=60)
-        # sold products
         
+        # sold products
         self.sold = Image.open("./images/sold.jpg")
         self.sold_Resize = self.sold.resize((200, 200), Image.ANTIALIAS)
         self.sold_Resize = ImageTk.PhotoImage(self.sold_Resize)
         self.soldLabel = tk.Button(self.activeFrame, image=self.sold_Resize, command=self.sold_products)
         self.soldLabel.place(x=285, y=60)
+
         # total revenue
-        
         self.revenue = Image.open("./images/revenue.jpg")
         self.revenue_Resize = self.revenue.resize((200, 200), Image.ANTIALIAS)
         self.revenue_Resize = ImageTk.PhotoImage(self.revenue_Resize)
         self.revenueLabel = tk.Button(self.activeFrame, image=self.revenue_Resize, command=self.total_revenue)
         self.revenueLabel.place(x=535, y=60)
+
         # total categories
-        
         self.category = Image.open("./images/category.jpg")
         self.category_Resize = self.category.resize((200, 200), Image.ANTIALIAS)
         self.category_Resize = ImageTk.PhotoImage(self.category_Resize)
         self.categoryLabel = tk.Button(self.activeFrame, image=self.category_Resize, command=self.total_categories)
         self.categoryLabel.place(x=785, y=60)
 
-    # show total products
+    # TODO: show total products
     def total_products(self):
+        # Establish the connection to the database
+        # and initialize a cursor object
+        db_connect = sqlite3.connect("store.db")
+        db_cursor = db_connect.cursor()
+
+        # Get the quantity of each item name in the inventory table
+        select_item_quantity_query = "SELECT stock from inventory"
+        db_cursor.execute(select_item_quantity_query)
         
-        tkinter.messagebox.showinfo("Total Products:")
-    # show total revenue
-    def total_revenue(self):
+        # all_item_quantity return an iteration of tuples 
+        # with quantity of each item name at the first index of each tuple
+        all_item_quantity = db_cursor.fetchall()
         
+        # Calculate the total products
+        # i[0] is the quantity of an item
+        products = 0
+        for i in all_item_quantity:
+            products += i[0]
+
+        tkinter.messagebox.showinfo("Total Products", f"Total Products in the inventory is: {products}")
+    
+    # TODO: show total revenue
+    def total_revenue(self):    
         tkinter.messagebox.showinfo("Total Revenue")
 
-    # show total categories
+    # TODO: show total categories
     def total_categories(self):
-        
         tkinter.messagebox.showinfo("Total Categories")
     
-    # show sold products
+    # TODO: show sold products
     def sold_products(self):
-            
-            tkinter.messagebox.showinfo("Sold Products")
+        tkinter.messagebox.showinfo("Sold Products")
     
 
         

f07ed62f75f4a03ad5b13948bb2789769487fb2d
Tonguesten36 <tung@tungsoft.net>
2023-04-17 17:13:36 +0700

    Added an important comment regarding self.state(zoomed)

--- app.py
+++ app.py
@@ -8,7 +8,11 @@ class GUI(Tk):
         self.width = self.winfo_screenwidth()
         self.height = self.winfo_screenheight()
         self.geometry("{width}x{height}+0+0".format(width=self.width, height=self.height))
+        
+        # This one only works on Windows,
+        #  if you want to run this on Linux or Mac, then change it to normal
         self.state("zoomed")
+        
         main_gui.FurnitureStore(self)
 
 # Driver code
